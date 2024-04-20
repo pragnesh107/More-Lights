499b864a2e5b528f5242ef4f721f4264d319c568
Pragnesh <pragneshmayani123@gmail.com>
2024-04-20 11:18:09 +0530

    change to main

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div>
     <Navbar />
     <Routes>
-        <Route path='' element={<Home />} />
+        <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />}/>
       <Route path='/gallery' element={<Gallery />}/>
     </Routes>
